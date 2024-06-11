b079de6deebad7aff69e49190657b8d67086b827
karkisarkar <prabin@nydoz.com>
2024-06-11 15:36:21 +0545

    career page added

--- functions.php
+++ functions.php
@@ -154,4 +154,102 @@ function change_role_name() {
 }
 add_action('init', 'change_role_name');
 
+
+function create_career_post_type() {
+    $labels = array(
+        'name'                  => _x( 'Careers', 'Post type general name', 'textdomain' ),
+        'singular_name'         => _x( 'Career', 'Post type singular name', 'textdomain' ),
+        'menu_name'             => _x( 'Careers', 'Admin Menu text', 'textdomain' ),
+        'name_admin_bar'        => _x( 'Career', 'Add New on Toolbar', 'textdomain' ),
+        'add_new'               => __( 'Add New', 'textdomain' ),
+        'add_new_item'          => __( 'Add New Career', 'textdomain' ),
+        'new_item'              => __( 'New Career', 'textdomain' ),
+        'edit_item'             => __( 'Edit Career', 'textdomain' ),
+        'view_item'             => __( 'View Career', 'textdomain' ),
+        'all_items'             => __( 'All Careers', 'textdomain' ),
+        'search_items'          => __( 'Search Careers', 'textdomain' ),
+        'parent_item_colon'     => __( 'Parent Careers:', 'textdomain' ),
+        'not_found'             => __( 'No careers found.', 'textdomain' ),
+        'not_found_in_trash'    => __( 'No careers found in Trash.', 'textdomain' ),
+        'featured_image'        => _x( 'Career Cover Image', 'Overrides the “Featured Image” phrase for this post type. Added in 4.3', 'textdomain' ),
+        'set_featured_image'    => _x( 'Set cover image', 'Overrides the “Set featured image” phrase for this post type. Added in 4.3', 'textdomain' ),
+        'remove_featured_image' => _x( 'Remove cover image', 'Overrides the “Remove featured image” phrase for this post type. Added in 4.3', 'textdomain' ),
+        'use_featured_image'    => _x( 'Use as cover image', 'Overrides the “Use as featured image” phrase for this post type. Added in 4.3', 'textdomain' ),
+        'archives'              => _x( 'Career archives', 'The post type archive label used in nav menus. Default “Post Archives”. Added in 4.4', 'textdomain' ),
+        'insert_into_item'      => _x( 'Insert into career', 'Overrides the “Insert into post”/”Insert into page” phrase (used when inserting media into a post). Added in 4.4', 'textdomain' ),
+        'uploaded_to_this_item' => _x( 'Uploaded to this career', 'Overrides the “Uploaded to this post”/”Uploaded to this page” phrase (used when viewing media attached to a post). Added in 4.4', 'textdomain' ),
+        'filter_items_list'     => _x( 'Filter careers list', 'Screen reader text for the filter links heading on the post type listing screen. Default “Filter posts list”/”Filter pages list”. Added in 4.4', 'textdomain' ),
+        'items_list_navigation' => _x( 'Careers list navigation', 'Screen reader text for the pagination heading on the post type listing screen. Default “Posts list navigation”/”Pages list navigation”. Added in 4.4', 'textdomain' ),
+        'items_list'            => _x( 'Careers list', 'Screen reader text for the items list heading on the post type listing screen. Default “Posts list”/”Pages list”. Added in 4.4', 'textdomain' ),
+    );
+
+    $args = array(
+        'labels'             => $labels,
+        'public'             => true,
+        'publicly_queryable' => true,
+        'show_ui'            => true,
+        'show_in_menu'       => true,
+        'query_var'          => true,
+        'rewrite'            => array( 'slug' => 'career' ),
+        'capability_type'    => 'post',
+        'has_archive'        => true,
+        'hierarchical'       => false,
+        'menu_position'      => null,
+        'supports'           => array( 'title', 'editor', 'author', 'thumbnail', 'excerpt', 'comments' ),
+        'taxonomies'         => array( 'category' ),
+    );
+
+    register_post_type( 'career', $args );
+}
+
+add_action( 'init', 'create_career_post_type' );
+
+function add_career_meta_boxes() {
+    add_meta_box(
+        'career_location',
+        'Location',
+        'career_location_callback',
+        'career',
+        'normal',
+        'high'
+    );
+    add_meta_box(
+        'career_company',
+        'Company Name',
+        'career_company_callback',
+        'career',
+        'normal',
+        'high'
+    );
+}
+
+add_action( 'add_meta_boxes', 'add_career_meta_boxes' );
+
+function career_location_callback( $post ) {
+    $value = get_post_meta( $post->ID, '_career_location', true );
+    echo '<label for="career_location">Location: </label>';
+    echo '<input type="text" id="career_location" name="career_location" value="' . esc_attr( $value ) . '" size="25" />';
+}
+
+function career_company_callback( $post ) {
+    $value = get_post_meta( $post->ID, '_career_company', true );
+    echo '<label for="career_company">Company Name: </label>';
+    echo '<input type="text" id="career_company" name="career_company" value="' . esc_attr( $value ) . '" size="25" />';
+}
+
+function save_career_meta_boxes_data( $post_id ) {
+    if ( ! isset( $_POST['career_location'] ) || ! isset( $_POST['career_company'] ) ) {
+        return;
+    }
+
+    $location = sanitize_text_field( $_POST['career_location'] );
+    $company = sanitize_text_field( $_POST['career_company'] );
+
+    update_post_meta( $post_id, '_career_location', $location );
+    update_post_meta( $post_id, '_career_company', $company );
+}
+
+add_action( 'save_post', 'save_career_meta_boxes_data' );
+
+
 ?>
